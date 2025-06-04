b15ddb65ad687ce68eab2c217560788d6550b281
Philippe Teuwen <phil@teuwen.org>
2025-06-04 18:29:59 +0200

    rough conversion to python3

--- scanner/main.py
+++ scanner/main.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 
 from TreeWalker import TreeWalker
 from CachePath import message
@@ -6,18 +6,15 @@ import sys
 import os
 
 def main():
-	reload(sys)
-	sys.setdefaultencoding("UTF-8")
+    if len(sys.argv) != 3:
+        print("usage: {} ALBUM_PATH CACHE_PATH".format(sys.argv[0]))
+        return
+    try:
+        os.umask(0o22)
+        TreeWalker(sys.argv[1], sys.argv[2])
+    except KeyboardInterrupt:
+        message("keyboard", "CTRL+C pressed, quitting.")
+        sys.exit(-97)
 
-	if len(sys.argv) != 3:
-		print "usage: %s ALBUM_PATH CACHE_PATH" % sys.argv[0]
-		return
-	try:
-		os.umask(022)
-		TreeWalker(sys.argv[1], sys.argv[2])
-	except KeyboardInterrupt:
-		message("keyboard", "CTRL+C pressed, quitting.")
-		sys.exit(-97)
-	
 if __name__ == "__main__":
-	main()
+    main()
--- serve.sh
+++ serve.sh
@@ -2,4 +2,4 @@
 
 cd web
 x-www-browser http://127.0.0.1:8000 &
-gnome-terminal -e "python -mSimpleHTTPServer"
+gnome-terminal -- python3 -m http.server
